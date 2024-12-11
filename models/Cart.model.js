787cf02c88252d8b768c72899a46621f2c74dd0d
john sebastian marmolejo castro <148784346+Lechattarre@users.noreply.github.com>
2024-12-11 12:00:41 +0100

    new cart

--- models/Cart.model.js
+++ models/Cart.model.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require("mongoose")
+const { Schema, model } = require("mongoose");
 
 const cartSchema = new Schema(
     {
@@ -6,19 +6,24 @@ const cartSchema = new Schema(
         items: [
             {
                 product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-                quantity: { type: Number, required: true, default: 1 }
+                quantity: { type: Number, required: true, default: 1 },
+                price: { type: Number, required: true }
             }
         ],
         status: {
             type: String,
-            enum: ['pending, finished']
-        }
+            enum: ['pending', 'finished'],
+            default: 'pending'
+        },
+        isPriceValid: { type: Number, default: 1.0 },
+        isTotalValid: { type: Number, default: 1.0 },
+        total: { type: Number, required: true, default: 0.0 }
     },
     {
         timestamps: true
     }
-)
+);
 
-const Cart = model("Cart", cartSchema)
+const Cart = model("Cart", cartSchema);
 
-module.exports = Cart
+module.exports = Cart;
